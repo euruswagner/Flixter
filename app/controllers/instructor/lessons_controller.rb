130a16b7baea14ea4aeb6e524ba740402d9cd7c0
Eurus Wagner <euruswagner@gmail.com>
2019-10-21 20:27:37 +0000

    Instructors can order Sections

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -19,10 +19,9 @@ class Instructor::LessonsController < ApplicationController
 
   private
 
-  def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
   end
 
   def current_lesson
@@ -35,11 +34,12 @@ class Instructor::LessonsController < ApplicationController
     end
   end
 
-  helper_method :current_section
-  def current_section
-    @current_section ||= Section.find(params[:section_id])
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
   end
-
+  
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
